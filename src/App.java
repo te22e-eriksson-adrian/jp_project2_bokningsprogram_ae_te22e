000c0cec09fe58fe4520c040c7a2e4f7dcb57a55
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-04-08 08:57:00 +0200

    lade till en meny som man navigerar genom att använda tangentbordet

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tangentbord = new Scanner(System.in);
+        int[] array = new int[20];
+
+        //Meny
+        System.out.println("Bokningssystem för bussresor:");
+        System.out.println("1. Lägga till en passagerare - boka en obokad plats \n2. Skriv ut antal lediga platser \n3. Beräkna vinsten av antalet sålda biljetter \n4. Avsluta programmet");
+        System.out.print("Inmata menyval här: ");
+        int val = tangentbord.nextInt();
+        tangentbord.nextLine();
+
+        tangentbord.close();
     }
 }
